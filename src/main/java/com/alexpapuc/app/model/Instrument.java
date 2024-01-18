daacad9aafe5bfca7dc94b0f83ce417a89fe419a
Alex Papuc <apapuc.dev@gmail.com>
2024-01-18 18:07:17 +0200

    Refactored unused enum value

--- src/main/java/com/alexpapuc/app/model/Instrument.java
+++ src/main/java/com/alexpapuc/app/model/Instrument.java
@@ -3,9 +3,7 @@ package com.alexpapuc.app.model;
 public enum Instrument {
     INSTRUMENT_1("INSTRUMENT1"),
     INSTRUMENT_2("INSTRUMENT2"),
-    INSTRUMENT_3("INSTRUMENT3"),
-    OTHER_INSTRUMENT(null);
-
+    INSTRUMENT_3("INSTRUMENT3");
     public final String name;
     Instrument(String name) {
         this.name = name;
